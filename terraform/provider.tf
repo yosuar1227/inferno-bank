19cca2968cb68895461e2043c527c2e79233538e
Yosuar Dev <yosuar.mieles@unicolombo.edu.co>
2025-09-11 18:11:54 -0500

    fix: delete public and secret key for amazon notification

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIA346NF4U7HNVPRDN5"
-    secret_key = "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"
+    access_key = "DELETED FOR GITHUB AND AMAZON NOTIFICATION"
+    secret_key = "DELETED FOR GITHUB AND AMAZON NOTIFICATION"
 }
